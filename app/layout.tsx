c649320d55b148ea68579395c342b9827f648bb9
temcryptodev <info@temcrypto.com>
2024-06-10 11:42:57 -0300

    Update the html lang to pt-BR by default

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="pt-BR" className="dark">
       <body className={`${nunito.className}`}>{children}</body>
     </html>
   );
